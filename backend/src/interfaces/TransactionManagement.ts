0cad9aec74540740f0e6de8e31f30185f9a04eee
Giuseppe <giuseppeuhlmann@gmail.com>
2022-11-17 22:22:58 -0300

    refactor: change return from transactions in backend

--- backend/src/interfaces/TransactionManagement.ts
+++ backend/src/interfaces/TransactionManagement.ts
@@ -1,9 +1,11 @@
 import { Decimal } from "@prisma/client/runtime";
 
 interface TransactionManagement {
+  id: number;
   name: string | undefined;
   value: Decimal;
-  data: string;
+  type: string;
+  date: string;
   hour: string;
 }
 
--- backend/src/utils/SerializeTransactions.ts
+++ backend/src/utils/SerializeTransactions.ts
@@ -1,33 +1,35 @@
 import { Transactions } from "@prisma/client";
+import TransactionManagement from "../interfaces/TransactionManagement";
 import UserService from "../services/UserService";
 
 const getData = (data: Date) => data.toLocaleDateString("pt-BR", {timeZone: "America/Sao_Paulo"});
 
-const getHours = (data: Date) => data.toLocaleTimeString("pt-BR", {timeZone: "America/Sao_Paulo"});
+const getHours = (data: Date) => data.toLocaleTimeString("pt-BR", {timeZone: "America/Sao_Paulo", hour: '2-digit', minute: '2-digit'});
 
 const serializeTransactions = async (transactions: Transactions[], id: number) => {
   const userService = new UserService();
-
 
   return await Promise.all(transactions.map(async (transaction) => {
     if (transaction.debitedAccountId !== id) {
       const debitedName = await userService.getUserById(transaction.debitedAccountId);
 
       return {
+        id: transaction.id,
         name: debitedName?.username,
         value: transaction.value,
-        type: 'cash-out',
-        data: getData(transaction.createdAt),
+        type: 'cash-in',
+        date: getData(transaction.createdAt),
         hour: getHours(transaction.createdAt),
       }
     } else {
       const creditedName = await userService.getUserById(transaction.creditedAccountId);
 
       return {
+        id: transaction.id,
         name: creditedName?.username,
         value: transaction.value,
-        type: 'cash-in',
-        data: getData(transaction.createdAt),
+        type: 'cash-out',
+        date: getData(transaction.createdAt),
         hour: getHours(transaction.createdAt),
       }
     }
